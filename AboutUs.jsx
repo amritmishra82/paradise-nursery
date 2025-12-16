a87c5d73bbef5617c59c87c4072e10359782452d
Mishra Amrit Lalji <115808810+amritmishra82@users.noreply.github.com>
2025-12-16 16:12:18 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,13 @@
+const AboutUs = () => {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery provides a wide variety of indoor and outdoor plants
+        to bring nature closer to your home.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
